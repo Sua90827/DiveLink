0e6003431e96ecb4c7019bb8c3b8793a3cbc2a5d
Sua Kim <kimsuasch@gmail.com>
2025-07-29 13:55:53 +0900

    The first commit

---
+++ src/main/java/com/divelink/server/DiveLinkApplication.java
@@ -0,0 +1,13 @@
+package com.divelink.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiveLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiveLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/divelink/server/DiveLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divelink.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiveLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
